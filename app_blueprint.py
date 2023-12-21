ca5dfcd07820cfb912a27c8ed4fb53cb4efd1229
Omar sherif <omar.sherif22@msa.edu.eg>
2023-12-21 16:29:37 +0200

    Tours retrieving information is working

--- app_blueprint.py
+++ app_blueprint.py
@@ -8,10 +8,6 @@ app_blueprint = Blueprint('app_blueprint',__name__)
 db = Database()
 operations = Operations()
 checkBoxes=3
-tours = [
-    {'Tour_operator':'Tiger Nixon', 'Date':'Jan 15, 2023, 10:00AM', 'Location':'Pyramids', 'Duration':'6 hours', 'Cost':'$50 per person','Availability':'7 spots'},
-    {'Tour_operator':'Yawan Kalidas', 'Date':'February 19, 2023, 8:00AM', 'Location':'Cairo', 'Duration':'3 hours', 'Cost':'$20 per person','Availability':'2 spots'}
-]
 
 @app_blueprint.route("/")
 def index():
@@ -28,6 +24,8 @@ def dashboard():
 
 @app_blueprint.route("/posts")
 def posts():
+    collection = db.get_collection('Tour')
+    tours = operations.Read(collection)
     return render_template("posts.html",tours=tours)
 
 @app_blueprint.route("/edit")
@@ -42,13 +40,6 @@ def new():
 def process_input():
     if request.method == "POST":
         return request.form
-
-@app_blueprint.route("/retrieve")
-def getInfo():
-    collection = db.get_collection('Tour')
-    result = operations.Read(collection)
-    result_json = json_util.dumps(result, default=str)
-    return jsonify(result_json)
 
 if __name__ == "__main__":
     app_blueprint.run(debug=True)
