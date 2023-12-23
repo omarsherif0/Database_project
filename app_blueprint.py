3ec2ef86be6e056fb9ee59f7fc4763246b27d2cf
Omar sherif <omar.sherif22@msa.edu.eg>
2023-12-23 02:17:12 +0200

    adapt guit, show image

--- app_blueprint.py
+++ app_blueprint.py
@@ -1,9 +1,9 @@
 from flask import Blueprint, render_template, request, jsonify
-from bson import json_util, Binary
-from io import BytesIO
-from PIL import Image
 from db import Database
+import base64
 from db_operations_ import Operations
+from PIL import Image
+from io import BytesIO
 
 app_blueprint = Blueprint('app_blueprint',__name__)
 
@@ -46,18 +46,20 @@ def edit():
     location = "Giza"
     query = {'Tour_operator': tour_operater, "Location": location}
     doc = collection.find_one(query)
-    image = Image.open(BytesIO(doc['image']))
-    print(doc)  
-    return render_template("edit.html", checkBoxes=checkBoxes, image=image)
+    binary_data = doc['image']
+    image2 = base64.b64encode(binary_data).decode('utf-8')
+    return render_template("edit.html", checkBoxes=checkBoxes, image2=image2)
 
 @app_blueprint.route("/new")
 def new():
     return render_template("new.html",checkBoxes=checkBoxes)
 
-@app_blueprint.route('/process_form', methods=['POST'])
+@app_blueprint.route('/process_form', methods=['GET'])
 def process_input():
-    if request.method == "POST":
-        return request.form
+    if request.method == 'GET':
+        form_data = dict(request.args)
+        return jsonify(form_data)
+    return jsonify({'error': 'Invalid request method'})
 
 if __name__ == "__main__":
     app_blueprint.run(debug=True)
